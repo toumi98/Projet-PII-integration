7ce9155baf9289eec052456f8b15dc53da3b6d64
toumi98 <mehdi.toumi@esprit.tn>
2025-02-17 00:33:39 +0100

    commit

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,6 +9,7 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
+use Doctrine\Common\Collections\Collection;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
